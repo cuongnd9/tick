98ac70c0ab6bbde5d3deaa0740df24bfcd60c02d
Cuong Duy Nguyen <cuongw.me@gmail.com>
2019-10-19 23:48:37 +0700

    Integrate UI Kitten (#3)

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
 import React, { useState } from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { ApplicationProvider } from 'react-native-ui-kitten';
+import { mapping, light as lightTheme } from '@eva-design/eva';
 import { Ionicons } from '@expo/vector-icons';
 
 import AppNavigator from './navigation/AppNavigator';
@@ -20,10 +22,12 @@ export default function App(props) {
     );
   } else {
     return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
+      <ApplicationProvider mapping={mapping} theme={lightTheme}>
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle='default' />}
+          <AppNavigator />
+        </View>
+      </ApplicationProvider>
     );
   }
 }
@@ -32,15 +36,15 @@ async function loadResourcesAsync() {
   await Promise.all([
     Asset.loadAsync([
       require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
+      require('./assets/images/robot-prod.png')
     ]),
     Font.loadAsync({
       // This is the font that we are using for our tab bar
       ...Ionicons.font,
       // We include SpaceMono because we use it in HomeScreen.js. Feel free to
       // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
+      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf')
+    })
   ]);
 }
 
@@ -57,6 +61,6 @@ function handleFinishLoading(setLoadingComplete) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: '#fff'
+  }
 });
