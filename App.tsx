3e3cdf9e944bcc1d3c42e9560d33c512775521b6
Cuong Duy Nguyen <cuongw.me@gmail.com>
2019-11-09 14:35:51 +0700

    Integrate React-Navigation (#23)

--- App.tsx
+++ App.tsx
@@ -1,19 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppContainer from './navigation/AppNavigator';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+function App() {
+  return <AppContainer />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
