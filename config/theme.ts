5ec1a7f94025a2a64295c1b4747ce9f8f84dc9a3
Cuong Duy Nguyen <cuongw.me@gmail.com>
2019-11-10 16:05:37 +0700

    Customize basic theme (#32)

--- config/theme.ts
+++ config/theme.ts
@@ -1,8 +1,5 @@
 export const color = {
   primary: '#ff7e67',
   secondary: '#07689f',
-  background: '#fafafa',
-  textLight: '#fafafa',
-  textDark: '#899bb4',
-  textDarker: '#2b2b2b'
+  background: '#fafafa'
 };
