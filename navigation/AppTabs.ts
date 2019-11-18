ef13aae5696066751b99be9227b19eb8fcb0ef62
Nguyen Duy Cuong <cuongw.me@gmail.com>
2019-11-18 23:53:18 +0700

    Bottom navigation icons

--- navigation/AppTabs.ts
+++ navigation/AppTabs.ts
@@ -1,5 +1,4 @@
 import { createBottomTabNavigator } from 'react-navigation-tabs';
-import { Icon } from 'react-native-ui-kitten';
 import { TaskScreen, AddTaskScreen, CategoryScreen } from '../screens';
 
 const AppTabs = createBottomTabNavigator(
@@ -8,29 +7,11 @@ const AppTabs = createBottomTabNavigator(
     'Add Task': AddTaskScreen,
     Category: CategoryScreen
   },
-  // {
-  //   defaultNavigationOptions: ({ navigation }) => ({
-  //     tabBarIcon: ({ focused, horizontal, tintColor }) => {
-  //       const { routeName } = navigation.state;
-  //       let iconName = '';
-  //       switch (routeName) {
-  //         case 'Task':
-  //           iconName = `a${focused ? '' : '-outline'}`;
-  //           break;
-  //         case 'Add Task':
-  //           iconName = `a${focused ? '' : '-outline'}`;
-  //           break;
-  //         default:
-  //           iconName = `a${focused ? '' : '-outline'}`;
-  //       }
-  //       return <Icon name={iconName} height={25} fill={tintColor} />;
-  //     }
-  //   }),
-  //   tabBarOptions: {
-  //     activeTintColor: 'tomato',
-  //     inactiveTintColor: 'gray'
-  //   }
-  // }
+  {
+    tabBarOptions: {
+      showIcon: true,
+    }
+  }
 );
 
 export default AppTabs;
--- screens/TaskScreen.tsx
+++ screens/TaskScreen.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Layout, Text } from 'react-native-ui-kitten'
+import { NavigationBottomTabScreenComponent } from 'react-navigation-tabs';
+import { Layout, Text, Icon } from 'react-native-ui-kitten';
 
-const TaskScreen: React.FC = () => {
+const TaskScreen: NavigationBottomTabScreenComponent = () => {
   return (
     <Layout>
       <Text>Task screen</Text>
     </Layout>
   );
+};
+
+TaskScreen.navigationOptions = {
+  tabBarIcon: ({ focused }) => (
+    <Icon
+      name={`bar-chart${focused ? '' : '-outline'}`}
+      height={25}
+      fill='green'
+    />
+  ),
 };
 
 export default TaskScreen;
