0cd1e48c8949e1722a1a1f69126ed0b354b0c0db
Nguyen Duy Cuong <cuongw.me@gmail.com>
2019-11-21 08:51:58 +0700

    Completete status bar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as GlobalLoading } from './GlobalLoading';
 export { default as GlobalNotification } from './GlobalNotification';
 export { default as TabBarIcon } from './TabBarIcon';
+export { default as SearchHeader } from './SearchHeader';
+export { default as StatusBar } from './StatusBar';
