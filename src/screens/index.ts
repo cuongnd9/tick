c145632e8d25d79c96b73695082d49cafcaae5a9
Nguyen Duy Cuong <cuongw.me@gmail.com>
2019-12-01 15:46:38 +0700

    🥧 Rename AddTask to NewTask

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,6 +2,6 @@ export { default as AuthLoadingScreen } from './AuthLoadingScreen';
 export { default as LoginScreen } from './LoginScreen';
 export { default as TaskScreen } from './TaskScreen';
 export { default as CategoryScreen } from './CategoryScreen';
-export { default as AddTaskScreen } from './AddTaskScreen';
+export { default as NewTaskScreen } from './NewTaskScreen';
 export { default as ProfileScreen } from './ProfileScreen';
 export { default as SearchTaskScreen } from './SearchTaskScreen';
