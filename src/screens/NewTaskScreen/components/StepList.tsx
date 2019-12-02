35c9da4789e7ce79d904427867c8392bfb74b105
Cuong Duy Nguyen <cuongw.me@gmail.com>
2019-12-02 23:13:43 +0700

    🌳 Complete logic for StepList component (#119)

--- src/screens/NewTaskScreen/components/StepList.tsx
+++ src/screens/NewTaskScreen/components/StepList.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
+import { thinid } from 'thinid';
 import AddStepItem from './AddStepItem';
 import StepItem from './StepItem';
 
 const StepList: React.FC = () => {
+  const [stepList, setStepList] = useState([]);
+  const handleRemoveItem = id => {
+    setStepList([...stepList].filter(step => step.id !== id));
+  };
   return (
     <View style={styles.container}>
-      <StepItem />
-      <AddStepItem />
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        data={stepList}
+        renderItem={({ item }: { item: any }) => (
+          <StepItem
+            id={item.id}
+            content={item.content}
+            onRemove={handleRemoveItem}
+          />
+        )}
+        keyExtractor={_ => thinid()}
+        extraData={stepList}
+      />
+      <AddStepItem
+        onAddStep={step =>
+          setStepList([...stepList, { id: thinid(), content: step }])
+        }
+      />
     </View>
   );
 };
