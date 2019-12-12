e2f8ede9899154b729f71dbd0a9c31b1cf92cf0b
Nguyen Duy Cuong <cuongw.me@gmail.com>
2019-12-12 18:06:00 +0700

    Complete Delete Task feature

--- src/screens/TaskScreen/components/TaskItem.tsx
+++ src/screens/TaskScreen/components/TaskItem.tsx
@@ -1,13 +1,19 @@
+import _ from 'lodash';
+import moment from 'moment';
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import { Icon, Text } from 'react-native-ui-kitten';
-import moment from 'moment';
-import _ from 'lodash';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 import { NavigationStackProp } from 'react-navigation-stack';
 import { color } from 'src/config/theme';
 import { taskStatus } from 'src/config/constants';
-import { Task, updateTaskAction, getTaskListAction } from 'src/models/task';
+import {
+  Task,
+  updateTaskAction,
+  getTaskListAction,
+  deleteTaskAction
+} from 'src/models/task';
 import { taskListType } from 'src/config/constants';
 import { categoryIcons, defaultCategoryIcon } from 'src/config/icons';
 
@@ -16,13 +22,15 @@ interface Props {
   task: Task;
   listType: string;
   onSelect?: Function;
+  onRemove?: Function;
 }
 
 const TaskItem: React.FC<Props> = ({
   navigation,
   listType,
   task,
-  onSelect
+  onSelect,
+  onRemove
 }) => {
   const dispatch = useDispatch();
   const [status, setStatus] = useState(task.status);
@@ -39,117 +47,146 @@ const TaskItem: React.FC<Props> = ({
           callback: () => dispatch(getTaskListAction({ isLoading: false }))
         })
       ),
-    10000
+    3000
   );
   const handleChangeStatus = () => {
     setStatus(status !== taskStatus.done ? taskStatus.done : taskStatus.todo);
     updateStatus();
   };
-  return (
-    <TouchableWithoutFeedback
-      onPress={() => navigation.navigate('EditTask', { task })}
-    >
-      <View style={styles.container}>
-        <View style={{ ...styles.contentContainer, marginBottom: 10 }}>
-          <TouchableWithoutFeedback onPress={handleChangeStatus}>
-            <View style={styles.titleContainer}>
-              <Icon
-                name={
-                  status === taskStatus.done
-                    ? 'checkmark-square-2-outline'
-                    : 'square-outline'
-                }
-                width={32}
-                height={32}
-                fill={color.primary}
-              />
-              <Text
-                numberOfLines={1}
-                ellipsizeMode='tail'
-                category='h4'
-                style={{
-                  ...styles.title,
-                  textDecorationLine:
-                    status === taskStatus.done ? 'line-through' : 'none'
-                }}
-              >
-                {task.title}
-              </Text>
-            </View>
-          </TouchableWithoutFeedback>
-          <View>
-            <Icon
-              name='star-outline'
-              width={24}
-              height={24}
-              fill={color.primary}
-            />
-          </View>
-        </View>
-        <View style={styles.contentContainer}>
-          <View style={styles.reminderContainer}>
-            <Text category='s2'>100%</Text>
-            <Icon
-              name='arrow-right'
-              width={19}
-              height={19}
-              fill={color.secondary}
-            />
-            <Icon
-              name='clock-outline'
-              width={19}
-              height={19}
-              fill={color.secondary}
-            />
-            <Text style={{ marginLeft: 2 }} category='s2'>
-              {listType === taskListType.today
-                ? moment(task.reminderDate).format('hh:mm A')
-                : moment(task.reminderDate).format('MMM Do hh:mm A')}
-            </Text>
-            <Icon
-              name='arrow-right'
-              width={19}
-              height={19}
-              fill={color.secondary}
-            />
-            <Icon
-              vis={false}
-              name='attach-outline'
-              width={19}
-              height={19}
-              fill={color.secondary}
-            />
-            <Icon
-              name='bell-outline'
-              width={19}
-              height={19}
-              fill={color.secondary}
-            />
-          </View>
-          <View style={styles.categoryContainer}>
-            <Icon
-              name={
-                _.get(
-                  categoryIcons.find(item =>
-                    item.nameList.includes(task.category.name.toLowerCase())
-                  ),
-                  'icon'
-                ) || defaultCategoryIcon
-              }
-              width={19}
-              height={19}
-              fill={color.secondary}
-            />
-            <Text>{task.category.name}</Text>
-          </View>
-        </View>
+  const handleDeleteTask = () => {
+    dispatch(
+      deleteTaskAction({
+        id: task.id
+      })
+    );
+    onRemove(task.id);
+  };
+  const renderDeleteButton = () => (
+    <TouchableWithoutFeedback onPress={handleDeleteTask}>
+      <View
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#FF3D71',
+          padding: 30,
+          marginBottom: 10,
+          marginLeft: 10,
+          borderRadius: 10
+        }}
+      >
+        <Icon name='trash' width={24} height={24} fill='#fff' />
       </View>
     </TouchableWithoutFeedback>
+  );
+  return (
+    <Swipeable renderRightActions={renderDeleteButton}>
+      <TouchableWithoutFeedback
+        onPress={() => {navigation.navigate('EditTask', { task }); onSelect(task.id)}}
+      >
+        <View style={styles.container}>
+          <View style={{ ...styles.contentContainer, marginBottom: 10 }}>
+            <TouchableWithoutFeedback onPress={handleChangeStatus}>
+              <View style={styles.titleContainer}>
+                <Icon
+                  name={
+                    status === taskStatus.done
+                      ? 'checkmark-square-2-outline'
+                      : 'square-outline'
+                  }
+                  width={32}
+                  height={32}
+                  fill={color.primary}
+                />
+                <Text
+                  numberOfLines={1}
+                  ellipsizeMode='tail'
+                  category='h4'
+                  style={{
+                    ...styles.title,
+                    textDecorationLine:
+                      status === taskStatus.done ? 'line-through' : 'none'
+                  }}
+                >
+                  {task.title}
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <View>
+              <Icon
+                name='star-outline'
+                width={24}
+                height={24}
+                fill={color.primary}
+              />
+            </View>
+          </View>
+          <View style={styles.contentContainer}>
+            <View style={styles.reminderContainer}>
+              <Text category='s2'>100%</Text>
+              <Icon
+                name='arrow-right'
+                width={19}
+                height={19}
+                fill={color.secondary}
+              />
+              <Icon
+                name='clock-outline'
+                width={19}
+                height={19}
+                fill={color.secondary}
+              />
+              <Text style={{ marginLeft: 2 }} category='s2'>
+                {listType === taskListType.today
+                  ? moment(task.reminderDate).format('hh:mm A')
+                  : moment(task.reminderDate).format('MMM Do hh:mm A')}
+              </Text>
+              <Icon
+                name='arrow-right'
+                width={19}
+                height={19}
+                fill={color.secondary}
+              />
+              <Icon
+                vis={false}
+                name='attach-outline'
+                width={19}
+                height={19}
+                fill={color.secondary}
+              />
+              <Icon
+                name='bell-outline'
+                width={19}
+                height={19}
+                fill={color.secondary}
+              />
+            </View>
+            <View style={styles.categoryContainer}>
+              <Icon
+                name={
+                  _.get(
+                    categoryIcons.find(item =>
+                      item.nameList.includes(task.category.name.toLowerCase())
+                    ),
+                    'icon'
+                  ) || defaultCategoryIcon
+                }
+                width={19}
+                height={19}
+                fill={color.secondary}
+              />
+              <Text>{task.category.name}</Text>
+            </View>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </Swipeable>
   );
 };
 
 TaskItem.defaultProps = {
-  onSelect: () => {}
+  onSelect: () => {},
+  onRemove: () => {}
 };
 
 const styles = StyleSheet.create({
